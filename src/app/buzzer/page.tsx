13bfd1872b285f0572554ac24b0caad553a72527
michael-m-2983 <michaelmccright2983@gmail.com>
2024-05-21 22:04:35 -0500

    Add clear buzzer button

--- src/app/buzzer/page.tsx
+++ src/app/buzzer/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button, Center, Stack, Text } from "@mantine/core";
+import { modals } from "@mantine/modals";
 import React from "react";
 import useSound from 'use-sound';
 
@@ -13,12 +14,22 @@ export default function BuzzerPage() {
         play();
     };
 
+    const clearBuzzer = () => {
+        modals.openConfirmModal({
+            title: "Are you sure you want to clear the buzzer?",
+            children: <p>If you do this without permission, you may not get to answer first.</p>,
+            labels: {confirm: "Yes", cancel: 'No'},
+            onConfirm: () => setBuzz(undefined)
+        });
+    };
+
     return <Center h="80vh">
         <Stack align="center">
             {buzz && <Text size="3em">Buzzed at {`${buzz.getMinutes()}:${buzz.getSeconds()}:${buzz.getMilliseconds()}`}</Text>}
             <Button className="buzzer" variant="gradient" size="4em" onClick={onClick} disabled={buzz != undefined}>
                 Buzzer!
             </Button>
+            {buzz && <Button variant="default" onClick={clearBuzzer}>Clear Buzzer</Button>}
         </Stack>
     </Center>
 };
